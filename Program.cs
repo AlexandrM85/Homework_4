3c26c6a1191f954ad050a849ede687756686180c
Alex <automex74@gmail.com>
2022-10-13 00:37:20 +0500

    add task_29

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-/*Console.WriteLine("--------------Задача №25.--------------");
+Console.WriteLine("--------------Задача №25.--------------");
 Console.Write("Введите число A: ");
 int A = int.Parse(Console.ReadLine()!);
 Console.Write("Введите число B: ");
@@ -15,7 +15,7 @@ int Exponentiation(int num)
     }
     return result;
 }
-Console.WriteLine($"Число {A} в {B} степени = {Exponentiation(A)}");*/
+Console.WriteLine($"Число {A} в {B} степени = {Exponentiation(A)}");
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 Console.WriteLine("--------------Задача №27.--------------");
@@ -33,4 +33,19 @@ int SumNumber(int num)
         sum = sum + num;
     }
     return sum;
+}
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.WriteLine("--------------Задача №29.--------------");
+int[] array = GetBinaryArray(8);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(15);
+    }
+    return result;
 }
